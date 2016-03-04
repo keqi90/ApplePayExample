ae67b4fd2af9c8be94c79e8f2c98e18927d76312
iwevon <iwevon@localhost>
2016-03-04 11:00:32 +0800

    Initial Commit

---
+++ ApplePayExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayExample
+//
+//  Created by iwevon on 16/3/4.
+//  Copyright © 2016年 iwevon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayExample
+//
+//  Created by iwevon on 16/3/4.
+//  Copyright © 2016年 iwevon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
